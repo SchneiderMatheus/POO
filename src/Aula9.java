7516cd05c93ca94dc80ce5120a1cb63c78a6bf0e
Matheus <matheuswschneider@gmail.com>
2026-06-05 17:15:47 -0300

    First Commit

---
+++ src/Aula9.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Aula9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        
+    }
+}
