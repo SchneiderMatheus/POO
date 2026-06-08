bafb9f3f8f8dcb5bc618aa93a55ff20cef9bdf24
Matheus <matheuswschneider@gmail.com>
2026-06-08 19:25:55 -0300

    sobrecarga

--- src/Aula9.java
+++ src/Aula9.java
@@ -14,7 +14,18 @@ public class Aula9 {
         palavra this = referencia para o proprio objeto
         -> diferenciar atributos de variaveis locais
         -> passar o proprio objeto como argumento na chamada de um metodo ou construtor
-        
+
+
+        Sobrecarga = é um recurso que uma classe poussiu de oferecer mais de uma operaçao com o mesmo nome,
+        porem com diferentes listas de paremetros;
+
+        proposta de melhoria no projeto atividadeConstrutores;
+
+        Vamos criar um contrutor opcional, o qual recebe apenas nome e preço do produto. A quantidade em estoque deste novo
+        produto, por padrão, é zero.
+
+        È possivel tbm incluir um construtor pedrão.
+
          */
 
         
