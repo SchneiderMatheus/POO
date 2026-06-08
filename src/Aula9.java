6a7c93eeb60009d6385728570440588ef4f3d554
Matheus <matheuswschneider@gmail.com>
2026-06-08 19:51:43 -0300

    encapsulamento

--- src/Aula9.java
+++ src/Aula9.java
@@ -26,6 +26,20 @@ public class Aula9 {
 
         È possivel tbm incluir um construtor pedrão.
 
+
+        Encapsulamento
+        • É um princípio que consiste em esconder detalhes de implementação de uma classe, expondo apenas
+        operações seguras e que mantenhamos objetos em um estado consistente.
+
+        • Regra de ouro: o objeto deve sempre estar em um estado consistente, e a própria classe deve garantir isso.
+
+        Regra geral básica
+        • Um objeto NÃO deve expor nenhum atributo (modificador de acesso
+        private)
+        • Os atributos devem ser acessados por meio de métodos get e set
+        • Padrão JavaBeans: https://en.wikipedia.org/wiki/JavaBeans
+
+
          */
 
         
