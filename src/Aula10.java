087677bfc8a087020d23939ddecd4d29306d6b27
Matheus <matheuswschneider@gmail.com>
2026-06-14 21:14:15 -0300

    aula10

--- src/Aula10.java
+++ src/Aula10.java
@@ -15,7 +15,7 @@ public class Aula10 {
 
             Tipos primitivos são tipos valor
             Em Java, tipos primitivos são tipos valor. Tipos valor são CAIXAS e não ponteiros.
-            exemplo: int x = 1 // cria uma caixa X e coloca o valor 10 ali dentro.
+            exemplo: int x = 1 // cria uma caixa X e coloca o valor 10 ali dentro(não uma referencia ao um heap).
 
             Valores padrão
             • Quando alocamos (new) qualquer tipo estruturado (classe ou array),
@@ -25,10 +25,21 @@ public class Aula10 {
             • char: caractere código 0
             • objeto: null
 
-
-
+            CLASSE                                //            PRIMITIVO
+Vantagem: usufrui de todos recursos OO            // Vantagem: é mais simples e mais performático
+Variáveis são ponteiros                           // Variáveis são caixas
+Objetos precisam ser instanciados usando new, ou  //Não instancia. Uma vez declarados, estão prontos
+apontar para um objeto já existente.              // para uso.
+Aceita valor null                                //Não aceita valor null
+Y = X; "Y passa a apontar para onde X aponta"    // Y =X "Y recebe uma cópia de X"
+Objetos instanciados no heap                     //"Objetos" instanciados no stack
+Objetos não utilizados são desalocados em um     // "Objetos" são desalocados imediatamente quando 
+momento próximo pelo garbage collector           // seu escopo de execução é finalizado 
 
         */
+
+            int p;
+            System.out.println(p); // not initialized
        sc.close();
     }
     
