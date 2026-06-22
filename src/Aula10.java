1524d4d90e36aa4e0f227106dd476775f5403913
Matheus <matheuswschneider@gmail.com>
2026-06-22 17:57:40 -0300

    Matriz

--- src/Aula10.java
+++ src/Aula10.java
@@ -193,7 +193,30 @@ momento próximo pelo garbage collector            // seu escopo de execução �
         • Dificuldade para se realizar inserções e deleções
 
          */
-        
+        int n = sc.nextInt();
+		int[][] mat = new int[n][n];
+		
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println("Main diagonal:");
+		for (int i=0; i<mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
+		}
+		System.out.println();
+		
+		int count = 0;
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat[i].length; j++) {
+				if (mat[i][j] < 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println("Negative numbers = " + count);
 
        sc.close();
     }
