368f4288b8eba5fcd0fcec08956a0624f3b8aae5
Matheus <matheuswschneider@gmail.com>
2026-06-23 22:53:18 -0300

    LocalDates

--- src/Aula11.java
+++ src/Aula11.java
@@ -1,6 +1,7 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Aula11 {
@@ -82,14 +83,25 @@ public class Aula11 {
             ZoneId
             ChronoUnit
 
-*/
+*/      
+        //DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+
         LocalDate d01 =LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
         Instant d03 = Instant.now();
 
         LocalDate d04 = LocalDate.parse("2022-07-20");
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
-        Instant d06 = Instant.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
+
+        LocalDate d08 = LocalDate.parse("20/07/2022", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        LocalDateTime d09 = LocalDateTime.parse("20/07/2022 01:30", fmt2);
+
+        LocalDate d10 = LocalDate.of(2022,7, 20);
+        LocalDateTime d11 = LocalDateTime.of(2022, 7, 20, 1, 30, 30);
 
         System.out.println("d01 = "+d01);
         System.out.println("d02 = "+d02);
@@ -97,5 +109,10 @@ public class Aula11 {
         System.out.println("d04 = "+d04);
         System.out.println("d05 = "+d05);
         System.out.println("d06 = "+d06);
+        System.out.println("d07 = "+d07);
+        System.out.println("d08 = "+d08);
+        System.out.println("d09 = "+d09);
+        System.out.println("d010 = "+d10);
+        System.out.println("d011 = "+d11);
     }
 }
