1f405108512223a2918a77b6f4a295590af8a2f7
Matheus <matheuswschneider@gmail.com>
2026-06-23 21:51:31 -0300

    Commit

--- src/Aula11.java
+++ src/Aula11.java
@@ -17,6 +17,7 @@ public class Aula11 {
         Uso comum: sistemas de região única, Excel.
         Data de nascimento: "15/06/2001"
         Data-hora da venda: "13/08/2022 às 15:32" (presumindo não interessar fuso horário) 
+
         • Data-hora global:
         Quando o momento exato interessa a pessoas de outro fuso horário.
         Uso comum: sistemas multi-região, web.
@@ -37,6 +38,33 @@ public class Aula11 {
         • "America/Sao_Paulo"
         • etc.
 
+        Padrão ISO 8601
+        Data-[hora] local:
+        2022-07-21
+        2022-07-21T14:52
+        2022-07-22T14:52:09
+        2022-07-22T14:52:09.4073
+        Data-hora global:
+        2022-07-23T14:52:09Z
+        2022-07-23T14:52:09.254935Z
+        2022-07-23T14:52:09-03:00
+
+
+        Operações importantes com data-hora
+        • Instanciação
+        • (agora) ➞ Data-hora
+        • Texto ISO 8601 ➞ Data-hora
+        • Texto formato customizado ➞ Data-hora
+        • dia, mês, ano, [horário] ➞ Data-hora local • Formatação
+        • Data-hora ➞ Texto ISO 8601
+        • Data-hora ➞ Texto formato customizado
+
+        Operações importantes com data-hora
+        • Converter data-hora global para local • Data-hora global, timezone (sistema local) ➞Data-hora local 
+        • Obter dados de uma data-hora local • Data-hora local ➞ dia, mês, ano, horário
+        • Cálculos com data-hora
+        • Data-hora +/- tempo ➞ Data-hora
+        • Data-hora 1, Data-hora 2 ➞ Duração
 
 */
     }
