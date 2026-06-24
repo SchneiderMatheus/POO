572271531cef534274e513a2c7a80c3117327c2a
Matheus <matheuswschneider@gmail.com>
2026-06-23 21:21:44 -0300

    ....

--- src/Aula11.java
+++ src/Aula11.java
@@ -16,7 +16,8 @@ public class Aula11 {
         Quando o momento exato não interessa a pessoas de outro fuso horário.
         Uso comum: sistemas de região única, Excel.
         Data de nascimento: "15/06/2001"
-        Data-hora da venda: "13/08/2022 às 15:32" (presumindo não interessar fuso horário) • Data-hora global:
+        Data-hora da venda: "13/08/2022 às 15:32" (presumindo não interessar fuso horário) 
+        • Data-hora global:
         Quando o momento exato interessa a pessoas de outro fuso horário.
         Uso comum: sistemas multi-região, web.
         Quando será o sorteio? "21/08/2022 às 20h (horário de São Paulo)"
@@ -24,7 +25,7 @@ public class Aula11 {
         Quando foi realizada a venda? "13/08/2022 às 15:32 (horário de São Paulo)"
         Início e fim do evento? "21/08/2022 às 14h até 16h (horário de São Paulo) 
 
-        
+
 */
     }
 }
