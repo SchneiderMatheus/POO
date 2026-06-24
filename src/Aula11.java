0fc141cc3a54c3c3885a309e803945f420f6bca2
Matheus <matheuswschneider@gmail.com>
2026-06-23 22:23:18 -0300

    Dates

--- src/Aula11.java
+++ src/Aula11.java
@@ -1,3 +1,4 @@
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Scanner;
@@ -84,8 +85,17 @@ public class Aula11 {
 */
         LocalDate d01 =LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
+
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26");
 
         System.out.println("d01 = "+d01);
         System.out.println("d02 = "+d02);
+        System.out.println("d03 = "+d03);
+        System.out.println("d04 = "+d04);
+        System.out.println("d05 = "+d05);
+        System.out.println("d06 = "+d06);
     }
 }
