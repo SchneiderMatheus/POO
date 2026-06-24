f7e5087ad7026df359e28049f868d2599c79b368
Matheus <matheuswschneider@gmail.com>
2026-06-23 21:49:34 -0300

    ..

--- src/Aula11.java
+++ src/Aula11.java
@@ -20,10 +20,22 @@ public class Aula11 {
         • Data-hora global:
         Quando o momento exato interessa a pessoas de outro fuso horário.
         Uso comum: sistemas multi-região, web.
+        2022-07-23T14:30:00Z
         Quando será o sorteio? "21/08/2022 às 20h (horário de São Paulo)"
         Quando o comentário foi postado? "há 17 minutos"
         Quando foi realizada a venda? "13/08/2022 às 15:32 (horário de São Paulo)"
         Início e fim do evento? "21/08/2022 às 14h até 16h (horário de São Paulo) 
+
+        Timezone (fuso horário) • GMT - Greenwich Mean Time
+        • Horário de Londres
+        • Horário do padrão UTC - Coordinated Universal Time
+        • Também chamado de "Z" time, ou Zulu time
+        • Outros fuso horários são relativos ao GMT/UTC: • São Paulo: GMT-3
+        • Manaus: GMT-4
+        • Portugal: GMT+1
+        • Muitas linguagens/tecnologias usam nomes para as timezones: • "US/Pacific"
+        • "America/Sao_Paulo"
+        • etc.
 
 
 */
