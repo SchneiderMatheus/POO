c19f8131793fa46a5a148498851ddb099cfbad74
Matheus <matheuswschneider@gmail.com>
2026-06-25 19:31:34 -0300

    aula

--- src/Aula11.java
+++ src/Aula11.java
@@ -1,6 +1,7 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
@@ -118,16 +119,20 @@ public class Aula11 {
         */ 
         LocalDate d04 = LocalDate.parse("2022-07-20");
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
-        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z"); //instant = hora global
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault()); //withZone(ZoneId.systemDefault())  isso puxa a data local
 
         System.out.println("d04 = "+d04.format(fmt1));
         System.out.println("d04 = "+fmt1.format(d04));
         System.out.println("d04 = "+d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 
+        System.out.println("d05 = "+d05.format(fmt1));
         System.out.println("d05 = "+d05.format(fmt2));
+
+        System.out.println("d06 = "+fmt3.format(d06)); // INSTANT NAO TEM .FORMAT ENTAO PRECISAMOS USAR O FMT PARA CHAMAR A FUNÇÃO .FORMAT;
 
 
 
