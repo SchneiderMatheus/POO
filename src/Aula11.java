1b9c83cea826b07a5f4de156e815ceb981b111ce
Matheus <matheuswschneider@gmail.com>
2026-06-25 19:24:53 -0300

    ....

--- src/Aula11.java
+++ src/Aula11.java
@@ -121,8 +121,13 @@ public class Aula11 {
         Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         System.out.println("d04 = "+d04.format(fmt1));
+        System.out.println("d04 = "+fmt1.format(d04));
+        System.out.println("d04 = "+d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+
+        System.out.println("d05 = "+d05.format(fmt2));
 
 
 
