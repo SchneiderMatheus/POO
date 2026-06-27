5d3614aba19eb26c6f6b8871b2916b9ebad4c8af
Matheus <matheuswschneider@gmail.com>
2026-06-27 15:02:01 -0300

    ....

--- src/Aula11.java
+++ src/Aula11.java
@@ -170,6 +170,7 @@ public class Aula11 {
         // Converter data-hora global para local (precisa informar o timezone);
         ZoneId.getAvailableZoneIds();
         LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault()); // convertendo uma data local de instant para data local do meu pc.
+        System.out.println(r1);
         sc.close();
     }
 }
