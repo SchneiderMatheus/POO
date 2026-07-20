0d57b703ee0705986a3ae84302c1fe49fa36660c
Matheus <matheuswschneider@gmail.com>
2026-07-20 17:52:24 -0300

    Aula 13

---
+++ src/aula13.java
@@ -0,0 +1,16 @@
+public class aula13 {
+    public static void main(String[] args) {
+        /* Herança
+        • É um tipo de associação que permite que uma classe herde todos dados e
+        comportamentos de outra
+        • Definições importantes
+        • Vantagens
+        • Reuso
+        • Polimorfismo
+        • Sintaxe
+        • class A extends B
+        
+        
+        */
+    }
+}
