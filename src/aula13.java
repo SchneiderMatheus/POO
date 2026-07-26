b5512034f3c2beca63053c4b58bf8760d8390b68
Matheus <matheuswschneider@gmail.com>
2026-07-26 18:13:13 -0300

    AULA 13

--- src/aula13.java
+++ src/aula13.java
@@ -10,6 +10,10 @@ public class aula13 {
         • Sintaxe
         • class A extends B
         
+        Suponha um negócio de banco que possui uma conta comum e uma conta para
+        empresas, sendo que a conta para empresa possui todos membros da conta
+        comum, mais um limite de empréstimo e uma operação de realizar empréstimo.
+
         
         */
     }
