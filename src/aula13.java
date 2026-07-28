8661c28dc0e87e9f238e12fc8b3e19ffc3980c96
Matheus <matheuswschneider@gmail.com>
2026-07-27 21:49:41 -0300

    ...

--- src/aula13.java
+++ src/aula13.java
@@ -32,11 +32,21 @@ public class aula13 {
         Upcasting
         • Casting da subclasse para superclasse
         • Uso comum: polimorfismo
-        
+
         Downcasting
         • Casting da superclasse para subclasse
         • Palavra instanceof
         • Uso comum: métodos que recebem parâmetros genéricos (ex: Equals)
-                */
+
+
+        Sobreposição ou sobrescrita
+        • É a implementação de um método de uma superclasse na subclasse
+        • É fortemente recomendável usar a
+        anotação @Override em um método
+        sobrescrito
+        • Facilita a leitura e compreensão do
+        código
+        • Avisamos ao compilador (boa prática)
+                        */
     }
 }
