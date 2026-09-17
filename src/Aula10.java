4aaeba5824fdf7ff696f96453074d17a8076b5fb
Matheus <matheuswschneider@gmail.com>
2026-09-17 17:33:18 -0300

    Review tipos referencia

--- src/Aula10.java
+++ src/Aula10.java
@@ -30,15 +30,15 @@ public class Aula10 {
             • objeto: null
 
             CLASSE                                //            PRIMITIVO
-Vantagem: usufrui de todos recursos OO            // Vantagem: é mais simples e mais performático
-Variáveis são ponteiros                           // Variáveis são caixas
-Objetos precisam ser instanciados usando new, ou  //Não instancia. Uma vez declarados, estão prontos
-apontar para um objeto já existente.              // para uso.
-Aceita valor null                                 //Não aceita valor null
-Y = X; "Y passa a apontar para onde X aponta"     // Y =X "Y recebe uma cópia de X"
-Objetos instanciados no heap                      //"Objetos" instanciados no stack
-Objetos não utilizados são desalocados em um      // "Objetos" são desalocados imediatamente quando 
-momento próximo pelo garbage collector            // seu escopo de execução é finalizado 
+        Vantagem: usufrui de todos recursos OO            // Vantagem: é mais simples e mais performático
+        Variáveis são ponteiros                           // Variáveis são caixas
+        Objetos precisam ser instanciados usando new, ou  //Não instancia. Uma vez declarados, estão prontos
+        apontar para um objeto já existente.              // para uso.
+        Aceita valor null                                 //Não aceita valor null
+        Y = X; "Y passa a apontar para onde X aponta"     // Y =X "Y recebe uma cópia de X"
+        Objetos instanciados no heap                      //"Objetos" instanciados no stack
+        Objetos não utilizados são desalocados em um      // "Objetos" são desalocados imediatamente quando 
+        momento próximo pelo garbage collector            // seu escopo de execução é finalizado 
 
         Desalocação de memória - garbage collector e escopo local
         Garbage collector
